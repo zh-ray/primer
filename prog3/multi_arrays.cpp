6745f5390270c030ce03efba9e5259d12ddcd42f
zhray <wxjiooio@gmail.com>
2018-10-28 15:18:23 +0800

    add third-dimensional array

--- prog3/multi_arrays.cpp
+++ prog3/multi_arrays.cpp
@@ -71,7 +71,7 @@ int main() {
         cout << endl;
     } */
 
-    int ia[3][4] = {
+    /* int ia[3][4] = {
         {0, 1, 2, 3},
         {4, 5, 6, 7},
         {8, 9, 10, 11}
@@ -110,6 +110,64 @@ int main() {
     for (refer_array *p = ia; p != ia + 3; ++p) {
         for (int *q = *p; q != *p + 4; ++q) {
             cout << *q << ' ';
+        }
+        cout << endl;
+    } */
+
+    int ia[2][3][4] = {
+        {
+            {0, 1, 2, 3},
+            {4, 5, 6, 7},
+            {8, 9, 10, 11},
+        },
+        {
+            {11, 10, 9, 8},
+            {7, 6, 5, 4},
+            {3, 2, 1, 0},
+        },
+    };
+
+    for (int (&p)[3][4] : ia) {
+        for (int (&q)[4] : p) {
+            for (int r : q) {
+                cout << r << ' ';
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (size_t i = 0; i != 2; ++i) {
+        for (size_t j = 0; j != 3; ++j) {
+            for (size_t k = 0; k != 4; ++k) {
+                cout << ia[i][j][k] << ' ';
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int (*p)[3][4] = ia; p != ia + 2; ++p) {
+        for (int (*q)[4] = *p; q != *p + 3; ++q) {
+            for (int *r = *q; r != *q + 4; ++r) {
+                cout << *r << ' ';
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    using third_array = int[3][4];
+    using second_array = int[4];
+    for (third_array &p : ia) {
+        for (second_array &q : p) {
+            for (int r : q) {
+                cout << r << ' ';
+            }
+            cout << endl;
         }
         cout << endl;
     }
